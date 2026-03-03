4e237ebdac4be84b7c1bd38caa7bc57c3db6f8ba
Dante Accinelli <Dante.Primon@t-systems.com>
2026-03-03 14:55:03 -0300

    feat: add enemy and player classes, and first combat system

---
+++ projects/c#/TextRPG/TextRPG/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG
+{
+    internal class Enemy
+    {
+        public string Name;
+        public int Health, MaxHealth, Attack;
+
+        public Enemy (string name, int maxHealth, int attack)
+        {
+            Name = name;
+            MaxHealth = maxHealth;
+            Health = maxHealth;
+            Attack = attack;
+        }
+
+        public void AttackPlayer(Player player)
+        {
+            Console.WriteLine($"{Name} attacks {player.Name} for {Attack} damage!");
+            player.TakeDamage(Attack);
+        }
+
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+
+            if (Health < 0) Health = 0;
+
+            Console.WriteLine($"{Name} takes {damage} damage!");
+            Console.WriteLine($"{Name}'s health: {Health}/{MaxHealth}");
+        }
+
+        public bool isAlive()
+        {
+            return Health > 0;
+        }
+    }
+}
--- projects/c#/TextRPG/TextRPG/Program.cs
+++ projects/c#/TextRPG/TextRPG/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,62 @@ namespace TextRPG
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("=== Text RPG ===");
+            Console.Write("Enter your Player Name: ");
+            string name = Console.ReadLine();
+
+            Player player = new Player(name);
+            Enemy goblin = new Enemy("Goblin", 30, 5);
+
+            Console.WriteLine("\nPlayer Created Successfully!");
+            Console.WriteLine($"Name: {player.Name}");
+            Console.WriteLine($"Health: {player.Health}/{player.MaxHealth}");
+            Console.WriteLine($"Attack: {player.Attack}");
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+
+            Console.WriteLine("\n------------------------------");
+            Console.WriteLine($"A {goblin.Name} appears!");
+
+            int round = 1;
+            while (player.isAlive() && goblin.isAlive())
+            {
+                Console.WriteLine($"\nRound {round}, Choose an action:");
+                Console.WriteLine("1 - Attack");
+                Console.WriteLine("2 - Do Nothing");
+                Console.Write("> ");
+                int opt = int.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+                switch(opt)
+                {
+                    case 1:
+                        player.AttackEnemy(goblin);
+                        break;
+                    case 2:
+                        Console.WriteLine($"{player.Name} does nothing...");
+                        break;
+                }
+                Console.WriteLine();
+
+                if (goblin.isAlive())
+                {
+                    goblin.AttackPlayer(player);
+                }
+                round++;
+            }
+
+            if (player.isAlive())
+            {
+                Console.WriteLine($"You defeated the {goblin.Name}!");
+            }
+            else
+            {
+                Console.WriteLine("You were defeated... Game Over.");
+            }
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
         }
     }
 }
