04ebbbe0e2fa35dc46097ecbc2fcb9ba41e17336
Dante Accinelli <Dante.Primon@t-systems.com>
2026-03-05 17:06:53 -0300

    feat: add orc special ability

--- projects/c#/TextRPG/TextRPG/Entities/Enemies/Orc.cs
+++ projects/c#/TextRPG/TextRPG/Entities/Enemies/Orc.cs
@@ -10,7 +10,26 @@ namespace TextRPG
     {
         public Orc() : base("Orc", 50, 8, 70)
         {
+        }
 
+        public override void AttackPlayer(Player player)
+        {
+            int heavyAttack = random.Next(1, 101);
+
+            if (heavyAttack <= 25)
+            {
+                int damage = Attack * 2;
+
+                Console.WriteLine($"{Name} performs a HEAVY ATTACK!");
+                Console.WriteLine($"{Name} deals {damage} damage!");
+
+                player.TakeDamage(damage);
+            }
+
+            else
+            {
+                base.AttackPlayer(player);
+            }
         }
     }
 }
